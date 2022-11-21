038a97089132d73a8d40c5f931bedf47a0c2e612
anastasia-chubukova <nastya.a.chubukova@gmail.com>
2022-11-21 21:55:54 +0100

    first commit

---
+++ LR_1/GameAccount.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Text;
+
+namespace LR_1
+{
+    public class GameAccount
+    {
+        public string UserName { get; set; }
+        public int CurrentRating
+        {
+            get
+            {
+                int Rating = 20;
+                foreach (var game in allGames)
+                {
+                    Rating += game.RatingGame;
+                }
+
+                return Rating;
+            }
+        }
+        public int GamesCount { get; set; }
+
+        public List<GameResult> allGames = new List<GameResult>();
+
+        public GameAccount(string name)
+        {
+            UserName = name;
+        }
+
+        public void WinGame(GameAccount opponentName, int rating)
+        {
+            if (rating <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(rating), "Rating can't be < 1");
+            }
+
+            this.GamesCount++;
+            GameResult Game = new GameResult(opponentName.UserName, 1, rating, DateTime.Now, GamesCount);
+            allGames.Add(Game);
+
+
+        }
+        public void LoseGame(GameAccount opponentName, int rating)
+        {
+            if (rating <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(rating), "Rating can't be < 1");
+            }
+            if (CurrentRating - rating <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(rating), "Rating can't be > rating of the player");
+            }
+            this.GamesCount++;
+            GameResult Game = new GameResult(opponentName.UserName, 0, -rating, DateTime.Now, GamesCount);
+            allGames.Add(Game);
+
+        }
+
+        public string GetStats()
+        {
+            var history = new System.Text.StringBuilder();
+
+            int currentRating = 20;
+            history.AppendLine("Player name:\tGame rating:\tResult:\t\tGame ID:\tGame Time:");
+            foreach (var game in allGames)
+            {
+                currentRating += game.RatingGame;
+                history.AppendLine($"{game.Opponetname}\t\t{currentRating}\t\t{game.Result}\t\t{game.ID}\t\t{DateTime.Now}");
+            }
+
+            return history.ToString();
+
+        }
+    }
+}
+
+
+
+
---
+++ LR_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LR_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            GameAccount firstPlayer = new GameAccount("Andrew");
+            GameAccount secondPlayer = new GameAccount("Den");
+
+            firstPlayer.WinGame(secondPlayer, 7);
+            secondPlayer.LoseGame(firstPlayer, 2);
+
+            firstPlayer.GetStats();
+            secondPlayer.GetStats();
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Game N1");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(firstPlayer.GetStats());
+            Console.WriteLine(secondPlayer.GetStats());
+
+            secondPlayer.WinGame(firstPlayer, 3);
+            firstPlayer.LoseGame(secondPlayer, 2);
+
+            secondPlayer.WinGame(firstPlayer, 10);
+            firstPlayer.LoseGame(secondPlayer, 6);
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Statistics of each player");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(firstPlayer.GetStats());
+            Console.WriteLine(secondPlayer.GetStats());
+        }
+    }
+}
